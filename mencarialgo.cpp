3b64f2a17b8203d0a2900815a176283828fe2a3a
Ilam Ball <informasiilam@gmail.com>
2024-04-04 15:19:09 +0700

    membuat perulangan for

--- mencarialgo.cpp
+++ mencarialgo.cpp
@@ -16,4 +16,13 @@ void input()
         else
             cout << "\nArray harus mempunyai minimal 1 dan maksimal 20 elemen.\n\n";
     }
+    
+    cout << "\n-----------------------\n";
+    cout << "Masukkan elemen array";
+    cout << "-----------------------\n";
+    for (i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
 }
